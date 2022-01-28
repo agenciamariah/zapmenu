4827d6f12424d9c62956958c9dc61ff6c32c3fc6
lucasbritoweb <lucasbritoweb@gmail.com>
2022-01-28 11:29:14 -0400

    ItemsController Last

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -270,8 +270,8 @@ class ItemsController extends Controller
         $thePOST = $request->all();
         //$itemToDuplicate = strip_tags($request->id);
         // Retrieve the first task
-        $ItemDuplicated = Items::first();
-        $ItemDuplicated = Items::findOrFail($thePOST['res_item_duplicar_id']); // pizza teste
+        $ItemDuplicated = Items::last();
+        //$ItemDuplicated = Items::findOrFail($thePOST['res_item_duplicar_id']); // pizza teste
 
         $newItemDuplicated = $ItemDuplicated->replicate();
         $newItemDuplicated->name = "Item Duplicado"; // the new project_id
