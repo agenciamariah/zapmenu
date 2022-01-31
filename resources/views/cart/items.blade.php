dc18ce057cb5801412d09b3cb5d445b74c0fa105
lucasbritoweb <lucasbritoweb@gmail.com>
2022-01-31 13:12:48 -0400

    Item type orçamento

--- resources/views/cart/items.blade.php
+++ resources/views/cart/items.blade.php
@@ -3,19 +3,30 @@
       <div class="mt-5">
         <h3>{{ __('Items') }}<span class="font-weight-light"></span></h3>
       </div>
+      <style type="text/css">
+      .block-item-type-2 .item-type-1 {
+        display: none;
+      }
+      .info-block .item-type-2 {
+        display: none;
+      }
+      .block-item-type-2 .item-type-2 {
+        display: block;
+      }
+      </style>
         <!-- List of items -->
         <div  id="cartList" class="border-top">
             <br />
             <div  v-for="item in items" class="items col-xs-12 col-sm-12 col-md-12 col-lg-12 clearfix">
-                <div class="info-block block-info clearfix" v-cloak>
+                <div class="info-block block-info block-item-type-@{{ item.attributes.item_orcamento }} clearfix" v-cloak>
                     <div class="square-box pull-left">
                     <figure>
                         <img :src="item.attributes.image" :data-src="item.attributes.image"  class="productImage" width="100" height="105" alt="">
                     </figure>
                     </div>
                     <h6 class="product-item_title">@{{ item.name }}</h6>
-                    <p class="product-item_quantity item-type-@{{ item.attributes.item_orcamento }}">@{{ item.quantity }} x @{{ item.attributes.friendly_price }}</p>
-                    <p class="product-item_quantity item-type-@{{ item.attributes.item_orcamento }}">@{{ item.quantity }} x Orçamento</p>
+                    <p class="product-item_quantity item-type-1">@{{ item.quantity }} x @{{ item.attributes.friendly_price }}</p>
+                    <p class="product-item_quantity item-type-2">@{{ item.quantity }} x Orçamento</p>
                     <div class="row">
                         <button type="button" v-on:click="decQuantity(item.id)" :value="item.id" class="btn btn-outline-primary btn-icon btn-sm page-link btn-cart-radius">
                             <span class="btn-inner--icon btn-cart-icon"><i class="fa fa-minus"></i></span>
