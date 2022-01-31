21a3bbdc29a488a122583ab0ae8982bc1ee02c58
lucasbritoweb <lucasbritoweb@gmail.com>
2022-01-31 18:10:43 -0400

    Rest Fix Menu #2

--- resources/views/restorants/show.blade.php
+++ resources/views/restorants/show.blade.php
@@ -315,7 +315,6 @@
 
 
     $('.new-price').each(function(){
-        $(this).attr('style', 'display: none');
         var textDot = $(this).html();
         console.log(textDot);
         textDot = textDot.replace(".", ",");
