42c73a6f093527650ed18793fdc197adc7fd5f8c
lucasbritoweb <lucasbritoweb@gmail.com>
2022-01-31 16:50:57 -0400

    Update Rest Ads #3

--- app/Restorant.php
+++ app/Restorant.php
@@ -16,7 +16,7 @@ class Restorant extends MyModel
     use HasConfig;
 
     protected $modelName="App\Restorant";
-    protected $fillable = ['name', 'subdomain', 'user_id', 'lat', 'lng', 'address', 'phone', 'logo', 'description', 'city_id'];
+    protected $fillable = ['name', 'subdomain', 'user_id', 'lat', 'lng', 'address', 'phone', 'logo', 'description', 'ad1_link', 'ad1_image', 'city_id'];
     protected $appends = ['alias', 'logom', 'icon', 'coverm'];
     protected $imagePath = '/uploads/restorants/';
 
