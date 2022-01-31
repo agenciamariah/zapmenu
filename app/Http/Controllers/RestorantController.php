17f7be555ffc9c12c67f386c0933071965a8a119
lucasbritoweb <lucasbritoweb@gmail.com>
2022-01-31 16:05:17 -0400

    Rest Footer AD link Controller

--- app/Http/Controllers/RestorantController.php
+++ app/Http/Controllers/RestorantController.php
@@ -312,6 +312,7 @@ class RestorantController extends Controller
         $restaurant->name = strip_tags($request->name);
         $restaurant->address = strip_tags($request->address);
         $restaurant->phone = strip_tags($request->phone);
+        $restaurant->ad1_link = strip_tags($request->ad1_link);
         
         $restaurant->description = strip_tags($request->description);
         $restaurant->minimum = strip_tags($request->minimum);
@@ -364,6 +365,14 @@ class RestorantController extends Controller
             $request->resto_wide_logo->move(public_path($this->imagePath), $uuid.'_original.'.'png');
             $restaurant->setConfig('resto_wide_logo',$uuid);
         }
+
+        if ($request->hasFile('ad1_image')) {
+       
+            $uuid = Str::uuid()->toString();
+            $request->ad1_image->move(public_path($this->imagePath), $uuid.'_original.'.'png');
+            $restaurant->setConfig('ad1_image',$uuid);
+        }
+
 
         if ($request->hasFile('resto_wide_logo_dark')) {
        
