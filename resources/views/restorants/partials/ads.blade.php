698bcb7e8c7d784f723083516b083059a855afca
lucasbritoweb <lucasbritoweb@gmail.com>
2022-01-31 15:54:38 -0400

    Rest Footer Admin ADS Tab

--- resources/views/restorants/partials/ads.blade.php
+++ resources/views/restorants/partials/ads.blade.php
@@ -1,4 +1,16 @@
-<div class="pl-lg-4">
+<div class="card card-profile bg-secondary shadow">
+    <div class="card-header">
+
+        <div class="row align-items-center">
+            <div class="col-8">
+                <h3 class="mb-0">Apps</h3>
+            </div>
+
+        </div>
+    </div>
+    <div class="card-body">
+
+    <div class="pl-lg-4">
     <form id="restorant-form" method="post" action="{{ route('admin.restaurants.update', $restorant) }}" autocomplete="off" enctype="multipart/form-data">
         @csrf
         @method('put')
@@ -44,4 +56,7 @@
         </div>
         
     </form>
+</div>
+
+</div>
 </div>
